59b589c32ef0610fb7221ae649b9b1a55dd1fbeb
Barath Vignarajah <moneyinthesky@gmail.com>
2022-07-31 17:43:33 +0100

    Separating animation logic

---
+++ animate.js
@@ -0,0 +1,40 @@
+import { waitingAsyncEventListener } from './async-events.js'
+
+export function prepareMove(element, x, y, startX) {
+    element.attr({ style: `--moveX: ${x}px; --moveY: ${y}px; --startX: ${startX}px` });
+}
+
+export function updateElementText(element, mapFunction, elementSize) {
+    let tSpan = element.children()[1].children();
+    let currentValue = tSpan.text()[0];
+
+    transformTextNode(element.children()[1], String(mapFunction(currentValue)), elementSize);
+}
+
+export function updateElementIndex(element, newIndex, elementSize) {
+    transformTextNode(element.children()[2], String(newIndex), elementSize);
+}
+
+export function updateElementColor(element, newColor) {
+    let container = element.children()[0];
+    container.fill(newColor);
+}
+
+export async function triggerAnimation(node, animationClass, keyframe) {
+    node.attr({ class: animationClass });
+    let event = await waitingAsyncEventListener(
+        'animationend', 
+        event => SVG(event.target) === node && event.animationName === keyframe
+    );
+    return event;
+}
+
+function transformTextNode(node, newValue, elementSize) {
+    node.children()[0].text(newValue);
+
+    let x = node.children()[0].attr().relativeX;
+    let xPosition = (elementSize - (newValue.length * 9.6)) / 2;
+    node.children()[0].attr({
+        x: x + xPosition
+    });
+}
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { configBuilder } from './config.js'
-import { waitingAsyncEventListener } from './async-events.js'
 import { ArrayDrawer } from './array-drawer.js'
+import { prepareMove, updateElementText, updateElementIndex, updateElementColor, triggerAnimation } from './animate.js'
 
 let array = ['apple', 'orange', 'banana'];
 let mapFunction = value => value.toUpperCase();
@@ -22,34 +22,42 @@ function drawPanel(id, methodFunction) {
     frameGroup.add(draw.rect(config.frameWidth, config.totalFrameHeight).fill(config.backgroundColor).attr({ rx: 10 }));
 
     frameGroup.add(draw.rect(config.arrayPanelWidth, config.totalFrameHeight).fill('#4DD0E1').attr({ x: 0, y: 0, rx: 10, stroke: '#0097A7', 'fill-opacity': 0.25 }));
-    frameGroup.add(buildText(draw, 'Original Array', config.arrayPanelWidth, config.infoFrameHeight, 0, config.totalFrameHeight - config.infoFrameHeight));
+    let originalArrayText = buildText(draw, 'Original Array');
+    center(originalArrayText, config.arrayPanelWidth, config.infoFrameHeight, 0, config.totalFrameHeight - config.infoFrameHeight);
+    frameGroup.add(originalArrayText);
     frameGroup.add(drawer.buildArrayGroup(array, `staticArray${id}`));
     
     frameGroup.add(draw.rect(config.arrayPanelWidth, config.totalFrameHeight).fill('#81C784').attr({ x: config.frameWidth - config.arrayPanelWidth, y: 0, rx: 10, stroke: '#388E3C', 'fill-opacity': 0.25 }));
-    frameGroup.add(buildText(draw, 'New Array', config.arrayPanelWidth, config.infoFrameHeight, config.frameWidth - config.arrayPanelWidth, config.totalFrameHeight - config.infoFrameHeight));
+    let newArrayText = buildText(draw, 'New Array');
+    center(newArrayText, config.arrayPanelWidth, config.infoFrameHeight, config.frameWidth - config.arrayPanelWidth, config.totalFrameHeight - config.infoFrameHeight);
+    frameGroup.add(newArrayText);
     frameGroup.add(drawer.buildArrayGroup(array, `movingArray${id}`, 0));
 
     frameGroup.add(draw.line(0, config.arrayFrameHeight, config.frameWidth, config.arrayFrameHeight).stroke({ width: 2, color: '#455A64' }));
 
     frameGroup.add(draw.rect(config.infoBoxWidth, config.infoBoxHeight).fill('#FFF176').attr({ x: config.arrayPanelWidth + config.bezelWidth, y: config.arrayFrameHeight + config.bezelWidth, rx: 10, stroke: 'black', 'fill-opacity': 0.5 }));
-    frameGroup.add(buildFunctionText(draw, methodFunction, config.infoBoxWidth, config.infoBoxHeight, config.arrayPanelWidth + config.bezelWidth, config.arrayFrameHeight + config.bezelWidth));
+    let functionText = buildFunctionText(draw, methodFunction)
+    center(functionText, config.infoBoxWidth, config.infoBoxHeight, config.arrayPanelWidth + config.bezelWidth, config.arrayFrameHeight + config.bezelWidth);
+    frameGroup.add(functionText);
 }
 
-function buildText(draw, value, containingWidth, containingHeight, xOffset, yOffset) {
-    return draw.text(value)
-        .font({ family: 'Helvetica', size: 18 })
-        .attr({ 
-            x: containingWidth / 2 + xOffset, 
-            y: containingHeight / 2 + yOffset,
-            'text-anchor': 'middle',
-            'dominant-baseline': 'middle'
-        });
+function center(element, containingWidth, containingHeight, xOffset, yOffset) {
+    element.attr({
+        'text-anchor': 'middle',
+        'dominant-baseline': 'middle',
+        x: containingWidth / 2 + xOffset, 
+        y: containingHeight / 2 + yOffset
+    });
 }
 
-function buildFunctionText(draw, value, containingWidth, containingHeight, xOffset, yOffset) {
-    let valueToShow = JSON.stringify(value, (key, val) => val + '');
+function buildText(draw, value) {
+    return draw.text(value).font({ family: 'Helvetica', size: 18 })
+}
+
+function buildFunctionText(draw, value) {
+    let valueToShow = JSON.stringify(value, (_, val) => val + '');
     valueToShow = valueToShow.slice(1, valueToShow.length - 1);
-    return draw.text(add => add.tspan(valueToShow).attr({ x: containingWidth / 2 + xOffset, y: containingHeight / 2 + yOffset }))
+    return draw.text(valueToShow)
         .fill('black')
         .attr({ 
             style: 'white-space: pre', 
@@ -57,8 +65,6 @@ function buildFunctionText(draw, value, containingWidth, containingHeight, xOffs
             'font-size': 16, 
             'font-weight': 'bold', 
             'letter-spacing': '0em',
-            'text-anchor': 'middle',
-            'dominant-baseline': 'middle'
         });
 }
 
@@ -71,7 +77,7 @@ async function runAnimation(mode, id) {
 
         if(mode === 'map') {
             updateElementColor(element, config.mappedArrayColor);
-            updateElementText(element);
+            updateElementText(element, mapFunction, config.elementSize);
 
             prepareMove(element, config.endPosition, 0, config.halfwayPosition);
             await triggerAnimation(element, 'animateMap', 'map');
@@ -80,7 +86,7 @@ async function runAnimation(mode, id) {
             let currentValue = tSpan.text()[0];
             if(filterFunction(currentValue)) {
                 updateElementColor(element, config.mappedArrayColor);
-                updateElementIndex(element, filteredElements);
+                updateElementIndex(element, filteredElements, config.elementSize);
 
                 prepareMove(element, config.endPosition, -((index - filteredElements) * (config.elementSize + config.elementSpacing)), config.halfwayPosition);
                 await triggerAnimation(element, 'animateMap', 'map');
@@ -90,43 +96,4 @@ async function runAnimation(mode, id) {
             }
         }
     }
-}
-
-function prepareMove(element, x, y, startX) {
-    element.attr({ style: `--moveX: ${x}px; --moveY: ${y}px; --startX: ${startX}px` });
-}
-
-function updateElementText(element) {
-    let tSpan = element.children()[1].children();
-    let currentValue = tSpan.text()[0];
-
-    transformTextNode(element.children()[1], String(mapFunction(currentValue)));
-}
-
-function updateElementIndex(element, newIndex) {
-    transformTextNode(element.children()[2], String(newIndex));
-}
-
-function updateElementColor(element, newColor) {
-    let container = element.children()[0];
-    container.fill(newColor);
-}
-
-async function triggerAnimation(node, animationClass, keyframe) {
-    node.attr({ class: animationClass });
-    let event = await waitingAsyncEventListener(
-        'animationend', 
-        event => SVG(event.target) === node && event.animationName === keyframe
-    );
-    return event;
-}
-
-function transformTextNode(node, newValue) {
-    node.children()[0].text(newValue);
-
-    let x = node.children()[0].attr().relativeX;
-    let xPosition = (config.elementSize - (newValue.length * 9.6)) / 2;
-    node.children()[0].attr({
-        x: x + xPosition
-    });
 }
